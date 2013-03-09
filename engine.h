2c508d190579b6056ec6028cae7a6daf5f33012a
Jon Ronen-Drori <jon_ronen@yahoo.com>
2013-03-09 21:11:06 +0200

    adding copyright and license

--- engine.h
+++ engine.h
@@ -1,3 +1,29 @@
+/*
+ *
+ * Arduino Toys
+ * Copyright 2012-2013 Jon Ronen-Drori
+ *
+ *
+ * An Arduino-based platform for children's toys
+ * with LEDs, push-buttons, and sounds
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *
+ */
+
+
 #ifndef __ENGINE_H__
 #define __ENGINE_H__
 

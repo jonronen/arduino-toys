49adfec2ef731d66b4843e08a82b016baeb2993f
Jon Ronen-Drori <jon_ronen@yahoo.com>
2013-05-02 21:52:28 +0300

    Make the repeat-me game a bit more friendly

--- code_samples/test6.asm
+++ code_samples/test6.asm
@@ -1,15 +1,30 @@
+:START
+# r9 holds the last random (to make sure the next random is different)
+# initialise it to an invalid value
+SET_REG r9, 0xff
+SET_REG r3, @SAVE_RAND_AND_PLAY
+JUMP r3
+
 :SAVE_RAND_AND_PLAY
 
 # get a random value between zero and four
 SET_REG r0, 5
 RANDOM r1, r0
 
+# if the random is equal to the last one, get another random
+SET_REG r3, @SAVE_RAND_AND_PLAY
+JUMP_EQUAL r1, r9, r3
+
+# else, set r9 to be the new random
+SET_REG r10, 0
+ADD_REGS r9, r1, r10
+
 # light and play
 LIGHT_ON r1
 PLAY_SOUND_BLOCKING r1
 LIGHT_OFF r1
 
-# save it
+# save the new random
 SET_REG r2, 0
 STORE r1, r2
 
@@ -21,47 +36,30 @@ JUMP r3
 :WAIT_FOR_INPUT
 
 # timeout is ten seconds
-SET_REG r1, 100
+SET_REG r1, 50
 GET_INPUT r8, r1
 
 # convert the saved value to an expected input
 LOAD r0, r2
-SET_REG r3, @CONVERT_INPUT
-JUMP r3
-
-
-:CONVERT_INPUT
-# r0 is a parameter
-SET_REG r1, 1
-SET_REG r2, 0
-SET_REG r3, @CONVERT_INPUT_LOOP
-JUMP r3
-
-:CONVERT_INPUT_LOOP
-# r0 is the limit, r1 is the accumulator, r2 is the index
-SET_REG r4, 2
-SET_REG r5, 1
-
-# if r2==r0, we're done and r1 is the answer
-SET_REG r3, @COMPARE_INPUT
-JUMP_EQUAL r2, r0, r3
-
-# else, r2++ and r1 *= 2
-MUL_REGS r1, r1, r4
-ADD_REGS r2, r2, r5
-
-# continue
-SET_REG r3, @CONVERT_INPUT_LOOP
-JUMP r3
-
-
-:COMPARE_INPUT
+BIT r1, r0
 
 # if r8==r1, success
 SET_REG r3, @SUCCESS_AND_CONTINUE
 JUMP_EQUAL r1, r8, r3
 
-# else, play an error sound, delay, and return to the start
+# else, if no button is pressed, let's give another chance
+SET_REG r1, 0
+SET_REG r3, @ANOTHER_CHANCE
+JUMP_EQUAL r8, r1, r3
+
+# else, error and continue
+SET_REG r3, @ERROR_AND_CONTINUE
+JUMP r3
+
+
+:ERROR_AND_CONTINUE
+
+# play an error sound, delay, and return to the start
 SET_REG r1, 5
 PLAY_SOUND_BLOCKING r1
 PLAY_SOUND_BLOCKING r1
@@ -71,7 +69,34 @@ SET_REG r3, @SAVE_RAND_AND_PLAY
 JUMP r3
 
 
+:ANOTHER_CHANCE
+
+# play the sound again
+LOAD r0, r2
+LIGHT_ON r0
+PLAY_SOUND_BLOCKING r0
+LIGHT_OFF r0
+
+# timeout is ten seconds
+SET_REG r1, 50
+GET_INPUT r8, r1
+
+# convert the saved value to an expected input
+LOAD r0, r2
+BIT r1, r0
+
+# if r8==r1, success
+SET_REG r3, @SUCCESS_AND_CONTINUE
+JUMP_EQUAL r1, r8, r3
+
+# else, error and continue
+SET_REG r3, @ERROR_AND_CONTINUE
+JUMP r3
+
+
 :SUCCESS_AND_CONTINUE
+# play it, delay, and choose a new random
+PLAY_SOUND_BLOCKING r0
 SET_REG r1, 8
 DELAY r1
 SET_REG r3, @SAVE_RAND_AND_PLAY
